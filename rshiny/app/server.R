3f08be8724fdb33f5404982d886ee9dc3e97a6e6
Ting An Lai <tlai28@wisc.edu>
2019-12-07 10:03:29 -0500

    update

--- rshiny/app/server.R
+++ rshiny/app/server.R
@@ -7,31 +7,13 @@ library(ggplot2)
 library(reticulate)
 library(keras)
 
-#import Python packages
-#py_run_string("import pandas as pd")
-#py_run_string("import numpy as np")
-#py_run_string("from sklearn.model_selection import train_test_split")
-#py_run_string("from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer")
-#py_run_string("from sklearn.linear_model import LogisticRegression")
-#py_run_string("import pickle")
 
-
-#py_run_string("loaded_model = pickle.load(open(filename, 'rb'))")
-#py_run_string("result = loaded_model.score(xtest_tfv, ytest)") #the input of x and y has to be already transformed
-#py_run_string("print(result)")
 
 #Load saved model
-#loaded_model = py_load_object("finalized_model.pkl") 
-
-model <- load_model_hdf5("My_model.h5")    # Load saved model
+model <- load_model_hdf5("My_model.h5")    
 tokenizer <- load_text_tokenizer('tokenizer')
 text_labels <- readRDS(file="text_labels.rds")
 
-
-source("featureMapping.R")                         
-#  a function for feature engineering. 
-#  You can include data imputation, data manipulation, data cleaning,
-#  feature transformation, etc.,  functions
 
 
 shinyServer(function(input, output) {
@@ -59,14 +41,16 @@ shinyServer(function(input, output) {
     if (is.null(inFile)){
       return(NULL)
     }else{
+      
+      #read uploaded data
       input_data =  readr::read_csv(input$file1$datapath, col_names = TRUE)
       
-      #colnames(input_data) = c("Test1", "Test2", "Label")
+      #rename columns
       colnames(input_data) = c("CustomerComplaint", "Label")
       
       input_data$Label = as.factor(input_data$Label )
       
-      #levels(input_data$Label) <- c("Failed", "Passed")
+      #rename y labels
       levels(input_data$Label) <- c("Checking or savings account", "Credit card or prepaid card", "Credit reporting, credit repair services, or other personal consumer reports", "Debt collection", "Money transfer, virtual currency, or money service", "Mortgage", "Payday loan, title loan, or personal loan", "Student loan", "Vehicle loan or lease")
       head(input_data)
     }
@@ -91,31 +75,30 @@ shinyServer(function(input, output) {
       withProgress(message = 'Predictions in progress. Please wait ...', {
         #load input data
         input_data =  readr::read_csv(input$file1$datapath, col_names = TRUE)
-        #py_run_string("input_data_py = pd.read_csv(""  
+        
+          
         input_data = na.omit(input_data)
-        #colnames(input_data) = c("Test1", "Test2", "Label")
+        
+        
         colnames(input_data) = c("CustomerComplaint", "Original_Product")
         
         
         input_data$Original_Product = as.factor(input_data$Original_Product)
         
-        #levels(input_data$Label) <- c("Failed", "Passed")
+        
         levels(input_data$Original_Product) <- c("Checking or savings account", "Credit card or prepaid card", "Credit reporting, credit repair services, or other personal consumer reports", "Debt collection", "Money transfer, virtual currency, or money service", "Mortgage", "Payday loan, title loan, or personal loan", "Student loan", "Vehicle loan or lease")
         
         
-        
+        #input model prep
         xtest = input_data$CustomerComplaint
         ytest = input_data$Original_Product
         
         
-        
+        #one hot encoding y prep function
         oneHot <- function(x) {
           xf <- factor(x)
           return(model.matrix(~xf+0))
-          
         }
-        
-        
         
         #transform xtest
         x_test <- texts_to_matrix(tokenizer, xtest, mode='tfidf')
@@ -124,20 +107,16 @@ shinyServer(function(input, output) {
         #transform ytest
         y_test<- oneHot(ytest)
         
-        #these three lines are for processing the model *****need to be revised
-        
-        #mapped = feature_mapping(input_data) #this is for previous person's feature engineering
-        #df_final = cbind(input_data, mapped) #this is for previous person's feature engineering
+        #call the keras sequential model from saved model
         test = predict(model, x_test)
-        #call out text label for preidction
         
+        #transform prediction label to text
         Predict_Labels <- c()
         for (i in 1:2000){
           Predict_Labels[i] <- text_labels[which(test[i,]==max(test[i,]))]
         }
-        #the prediction demostration table
-        ##py_run_string("print()")
-        
+
+        #ultimate output
         input_data_with_prediction = cbind(input_data, Predict_Labels) 
         input_data_with_prediction
         
@@ -158,7 +137,7 @@ shinyServer(function(input, output) {
     }
   })
   
-  output$sample_predictions = renderTable({   # the last 6 rows to show
+  output$sample_predictions = renderTable({   # generate the output table, last 6 rows to show
     pred = predictions()
     head(pred)
     
